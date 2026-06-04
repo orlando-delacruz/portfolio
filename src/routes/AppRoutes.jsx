6c10c3d805b9003f9b37b2576357996e95365c17
orly07 <orlando.delacruz.dev@gmail.com>
2026-06-04 17:29:46 +0800

    add: page name for coming soon

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,9 +7,9 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<ComingSoon />} />
-      <Route path="/projects" element={<ComingSoon />} />
-      <Route path="/blogs" element={<ComingSoon />} />
+      <Route path="/about" element={<ComingSoon pageName="About Page" />} />
+      <Route path="/projects" element={<ComingSoon pageName="Project Page" />} />
+      <Route path="/blogs" element={<ComingSoon pageName="Blog Page" />} />
     </Routes>
   );
 }
